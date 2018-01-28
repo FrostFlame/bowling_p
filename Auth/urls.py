2e2b0620ab70142e42c836ed2e3c3b90ef118528
safin972 <kair493@mail.ru>
2018-01-28 12:35:43 +0300

    12-login

--- Auth/urls.py
+++ Auth/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from Auth.views import RegisterUserView
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     url(r'register/$', RegisterUserView.as_view(), name="register"),
+    url(r'login/$',LoginView.as_view(template_name='Auth/login.html'), name="login"),
+
 ]
