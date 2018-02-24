37c5467ea8335f6c6b5b2b13566876fc7c06e996
Sagit Khaliullin <haliullinsagit@fosslabs.ru>
2018-02-25 01:15:38 +0300

    Tournament name regexp fixed

--- tournaments/forms.py
+++ tournaments/forms.py
@@ -24,7 +24,7 @@ class TournamentCreationForm(forms.ModelForm):
         label='Название',
         widget=forms.TextInput(
             attrs={'placeholder': 'Название', 'class': 'form-control', 'data-validation': 'custom',
-                   'data-validation-regexp': '^[a-zA-Zа-яА-Я]+$'}
+                   'data-validation-regexp': '^[а-яёА-ЯЁa-zA-Z0-9"]+([\s]?[а-яёА-ЯЁa-zA-Z0-9"]+)*$'}
         )
     )
 
