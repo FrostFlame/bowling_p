1c8c832ed30e234f6f5bb9f88b9f3684de64afe6
FrostFlame <serazetdinow.damir@yandex.ru>
2018-03-10 19:57:13 +0300

    list of news with pagination added

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from news.views import NewsCreateView, NewsView
+from news.views import NewsCreateView, NewsView, AllNewsView
 
 urlpatterns = [
     url(r'^create$', NewsCreateView.as_view(), name='news_create'),
-    url(r'^(?P<pk>\d+)$', NewsView.as_view(), name='news_view')
+    url(r'^(?P<pk>\d+)$', NewsView.as_view(), name='news_view'),
+    url(r'^all$', AllNewsView.as_view(), name='news_list'),
 ]
--- news/views.py
+++ news/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, ListView
 
 from news.forms import NewsCreationForm
 from news.models import News
@@ -23,3 +23,9 @@ class NewsView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
+class AllNewsView(ListView):
+    queryset = News.objects.all().order_by('created')
+    paginate_by = 10
+    template_name = 'news/news_list.html'
+    context_object_name = 'news'
