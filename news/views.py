a1f13a2fc167da380af7248ea9b9f5c01642c74e
FrostFlame <serazetdinow.damir@yandex.ru>
2018-03-10 22:04:12 +0300

    news edit added

--- news/views.py
+++ news/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
 
 from news.forms import NewsCreationForm
 from news.models import News
@@ -17,6 +17,7 @@ class NewsCreateView(CreateView):
     success_url = get_success_url
     form_class = NewsCreationForm
 
+
 class NewsView(DetailView):
     model = News
 
@@ -24,8 +25,19 @@ class NewsView(DetailView):
         context = super().get_context_data(**kwargs)
         return context
 
+
 class AllNewsView(ListView):
     queryset = News.objects.all().order_by('created')
     paginate_by = 10
     template_name = 'news/news_list.html'
     context_object_name = 'news'
+
+
+class NewsUpdate(UpdateView):
+    def get_success_url(self):
+        return reverse('news:news_view', args=(self.object.id,))
+
+    model = News
+    template_name = 'news/news_create.html'
+    success_url = get_success_url
+    form_class = NewsCreationForm
