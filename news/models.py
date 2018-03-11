dba7e60c621331c3bacd90d11e052cb157aaaa49
Sagit Khaliullin <haliullinsagit@fosslabs.ru>
2018-03-11 21:45:19 +0300

    News title length changed. UI added

--- news/models.py
+++ news/models.py
@@ -5,7 +5,7 @@ from accounts.utils import UploadToPathAndRename
 
 
 class News(models.Model):
-    title = models.CharField(max_length=40)
+    title = models.CharField(max_length=140)
     text = models.TextField(max_length=500)
     created = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to=UploadToPathAndRename('news/'))
