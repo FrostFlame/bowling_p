2f664a8bc5366af2501c2585df38758f326d2514
safin972 <kair493@mail.ru>
2018-03-16 22:38:20 +0300

    + event add form

--- bowling_app/forms.py
+++ bowling_app/forms.py
@@ -2,6 +2,7 @@ from dal import autocomplete
 from django import forms
 
 from accounts.models import PlayerInfo, SportCategory, SEX_CHOICES, City
+from bowling_app.models import Event
 
 
 class StaffPlayerRegister(forms.ModelForm):
@@ -82,3 +83,30 @@ class StaffPlayerRegister(forms.ModelForm):
         if commit:
             player.save()
         return player
+
+class EventCreationForm (forms.ModelForm):
+
+    name = forms.CharField(
+        label='Название',
+        widget=forms.TextInput(
+            attrs={'class': 'form-control'}
+        )
+    )
+    date = forms.DateTimeField(
+        label='Время мероприятия',
+        widget=forms.TextInput(
+            attrs={'class': 'form-control', 'data-validation': 'custom',
+                   'data-validation-regexp': '^(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})$'}
+        )
+    )
+
+    description = forms.CharField(
+        label="Описание",
+        widget=forms.Textarea(
+            attrs={'class': 'form-control', 'rows': '4'}
+        )
+    )
+
+    class Meta:
+        model=Event
+        fields=('name','date','description')
